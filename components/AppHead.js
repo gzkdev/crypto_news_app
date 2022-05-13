c7c56f8e2a42040ace96a6c262e7af7c32b7a5dc
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-13 14:21:41 +0100

    updated app structure

---
+++ components/AppHead.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+
+export default function AppHead() {
+    return (
+        <Head>
+            <title>Cypto news app</title>
+            <meta name="description" content="Web application to get current crypto news" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
